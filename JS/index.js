933bfa6d2dc63a39789b067e04498325133f0870
MrPenguino26 <69403205+MrPenguino26@users.noreply.github.com>
2021-11-27 11:48:40 -0800

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('menu', function()) {
+  document.getElementById("demo").innerHTML = "Hello"
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="events.html">Events</a></li>
+      </ul>
+    </div>
+  </nav>
+  <ul class="sidenav" id="mobile-demo">
+  <li><a href="events.html">CV</a></li>
